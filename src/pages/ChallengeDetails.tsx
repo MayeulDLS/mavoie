9f488d6b3cfa4479ccd22b4f319cbeb436419041
MayeultheLink <mdelaserre@hotmail.fr>
2025-12-15 17:55:06 +0100

    page détails

--- src/pages/ChallengeDetails.tsx
+++ src/pages/ChallengeDetails.tsx
@@ -1,3 +1,23 @@
+import { Card, Typography } from '@mui/material'
+import datas from '../data/challenges.json'
+import { useParams } from 'react-router-dom'
+
 export default function ChallengeDetails() {
-    return <></>
+    const { id } = useParams()
+    const challenge = datas.challenges.find((challenge) => challenge.id === id)
+
+    if (!challenge) {
+        return <div>Challenge not found</div>
+    }
+
+    return (
+        <Card>
+            <Typography variant="h3">{challenge.title}</Typography>
+            <Typography variant="h5">{challenge.duration}</Typography>
+            <Typography variant="body1">{challenge.longDescription}</Typography>
+            {challenge.requirements.map((requirement) => (
+                <Typography variant="body2">{requirement}</Typography>
+            ))}
+        </Card>
+    )
 }
