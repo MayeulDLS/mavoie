341efd9886ea292fffadf74d820d1a771fe8e682
MayeultheLink <mdelaserre@hotmail.fr>
2025-12-15 19:16:09 +0100

    UX

--- src/pages/ChallengeDetails.tsx
+++ src/pages/ChallengeDetails.tsx
@@ -1,47 +1,101 @@
-import { Card, Typography } from '@mui/material'
+import { Card, Container, IconButton, Typography, Box } from '@mui/material'
 import datas from '../data/challenges.json'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Duration from '../components/Duration'
+import { ArrowBack, ArrowForward } from '@mui/icons-material'
+import { useState } from 'react'
 
 export default function ChallengeDetails() {
-    const { id } = useParams()
-    const challenge = datas.challenges.find((challenge) => challenge.id === id)
+    const navigate = useNavigate()
+    const handleBackClick = () => {
+        navigate('/')
+    }
 
-    if (!challenge) {
+    const { id } = useParams()
+    const initialIndex = datas.challenges.findIndex((c) => c.id === id)
+    const [currentIndex, setCurrentIndex] = useState(
+        initialIndex >= 0 ? initialIndex : 0
+    )
+    if (initialIndex === -1) {
         return <div>Challenge not found</div>
+    }
+    const challenge = datas.challenges[currentIndex]
+
+    const previousCard = () => {
+        setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev))
+    }
+    const nextCard = () => {
+        setCurrentIndex((prev) =>
+            prev < datas.challenges.length - 1 ? prev + 1 : prev
+        )
     }
 
     return (
-        <Card
-            sx={{
-                width: '50%',
-                margin: 'auto',
-                padding: 4,
-                marginTop: 4,
-                borderRadius: 2,
-                border: '1px solid #ccc',
-            }}
-        >
-            <Typography
-                variant="h3"
+        <Container>
+            <IconButton
+                onClick={handleBackClick}
+                sx={{ position: 'fixed', top: 16, left: 16 }}
+            >
+                <ArrowBack />
+            </IconButton>
+
+            <Box
                 sx={{
-                    fontSize: '2rem',
-                    fontWeight: 'bold',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    width: '50%',
+                    margin: '16px auto 0',
                 }}
             >
-                {challenge.title}
-            </Typography>
-            <Duration duration={challenge.duration} />
-            <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 2 }}>
-                Descriptif
-            </Typography>
-            <Typography variant="body1">{challenge.longDescription}</Typography>
-            <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 2 }}>
-                Avant de te lancer
-            </Typography>
-            {challenge.requirements.map((requirement) => (
-                <Typography variant="body2">{requirement}</Typography>
-            ))}
-        </Card>
+                <IconButton
+                    onClick={previousCard}
+                    disabled={currentIndex === 0}
+                >
+                    <ArrowBack />
+                </IconButton>
+                <IconButton
+                    onClick={nextCard}
+                    disabled={currentIndex === datas.challenges.length - 1}
+                >
+                    <ArrowForward />
+                </IconButton>
+            </Box>
+
+            <Card
+                sx={{
+                    width: '50%',
+                    margin: 'auto',
+                    padding: 4,
+                    marginTop: 2,
+                    borderRadius: 2,
+                    border: '1px solid #ccc',
+                }}
+            >
+                <Typography
+                    variant="h3"
+                    sx={{
+                        fontSize: '2rem',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    {challenge.title}
+                </Typography>
+                <Duration duration={challenge.duration} />
+                <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 2 }}>
+                    Descriptif
+                </Typography>
+                <Typography variant="body1">
+                    {challenge.longDescription}
+                </Typography>
+                <Typography variant="h6" sx={{ marginTop: 2, marginBottom: 2 }}>
+                    Avant de te lancer
+                </Typography>
+                {challenge.requirements.map((requirement, idx) => (
+                    <Typography key={idx} variant="body2">
+                        {requirement}
+                    </Typography>
+                ))}
+            </Card>
+        </Container>
     )
 }
