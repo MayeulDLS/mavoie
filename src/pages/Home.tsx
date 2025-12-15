4cc589660621e34c331232c40fa78958212a731d
MayeultheLink <mdelaserre@hotmail.fr>
2025-12-15 19:24:57 +0100

    home page responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import Header from '../components/Header'
 import ChallengeCard from '../components/ChallengeCard'
 import datas from '../data/challenges.json'
-import { Container, Toolbar } from '@mui/material'
+import { Container, Grid, Toolbar } from '@mui/material'
 
 export default function Home() {
     return (
         <Container>
             <Header />
             <Toolbar />
-            {datas.challenges.map((challenge) => (
-                <ChallengeCard key={challenge.id} challenge={challenge} />
-            ))}
+            <Grid container spacing={2}>
+                {datas.challenges.map((challenge) => (
+                    <Grid key={challenge.id} size={{ xs: 12, sm: 6, md: 4 }}>
+                        <ChallengeCard challenge={challenge} />
+                    </Grid>
+                ))}
+            </Grid>
         </Container>
     )
 }
